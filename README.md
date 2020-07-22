29b41742953d4dd148277750805edfc1cc500c1c
daoudnazek <dawood_nazek_92@hotmail.com>
2020-07-22 08:38:05 +0300

    complete two tasks

--- README.md
+++ README.md
@@ -20,3 +20,26 @@ Start time: 3:35
 Finish time: 
 
 Actual time needed to complete: 
+
+# lab-03
+
+Number and name of feature: Feature 1: Pagination
+
+Estimate of time needed to complete: 50 mins
+
+Start time: 2:50
+
+Finish time: 5:10
+
+Actual time needed to complete: 2 hours and 20 mins
+
+
+Number and name of feature: Feature 1: Templating
+
+Estimate of time needed to complete: 50 mins
+
+Start time: 6:00
+
+Finish time: 6:30
+
+Actual time needed to complete: 30 mins
--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
 'use strict';
 
-$.get('data/page-1.json').then(data => {
-    data.forEach(element => {
-        let horns = new Horns(element.image_url, element.title, element.description, element.keyword, element.horns)
-        horns.render();
-        addOptions();
-    });
-});
+function switchPages(numberOfPage){
+    $(`.page-${numberOfPage}`).on("click",function(){
+        $('main').empty();
+        $('select').empty();
+        $.get(`data/page-${numberOfPage}.json`).then(data => {
+            data.forEach(element => {
+                let horns = new Horns(element.image_url, element.title, element.description, element.keyword, element.horns)
+                horns.render();
+                addOptions();
+            });
+        });
+    })
+}
+switchPages(1);
+switchPages(2);
+
+
 
 var hornsArr = [];
 var optionsArr = [];
@@ -21,13 +31,9 @@ function Horns(image_url, title, description, keyword, horns) {
 };
 
 Horns.prototype.render = function () {
-    let itemClone = $('.photo-template').clone();
-    itemClone.removeClass('photo-template');
-    itemClone.attr('class',this.keyword);
-    itemClone.find('img').attr("src", this.image_url);
-    itemClone.find('h2').text(this.title);
-    itemClone.find('p').text(this.description);
-    $('main').append(itemClone);
+    let mustachTemplate =$('#horns-template').html();
+    let newItem = Mustache.render(mustachTemplate,this);
+    $('main').append(newItem);
 };
 
 function addOptions (){
@@ -39,6 +45,16 @@ function addOptions (){
       });
 }
 
+function sortImagesByKeyword(){
+    hornsArr.sort((a,b) => a.keyword.localCompare(b.keyword))
+}
+
+function sortImagesByNumberOfHorns(){
+    hornsArr.sort((a,b) => a.horns - b.horns);
+}
+
+
+
 
 function showSelectedOption() {
   $('select').change(function () {
@@ -49,3 +65,4 @@ function showSelectedOption() {
 }
 
 showSelectedOption();
+
