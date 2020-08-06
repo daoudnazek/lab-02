1efd7b00da373499ca7691b09cde48c7cb69596f
daoudnazek <dawood_nazek_92@hotmail.com>
2020-08-06 06:25:37 +0300

    edit lab033

--- js/app.js
+++ js/app.js
@@ -1,18 +1,26 @@
 'use strict';
 
-
-$.get('data/page-1.json').then(data => {
-    data.forEach(element => {
-        let horns = new Horns(element.image_url, element.title, element.description, element.keyword, element.horns)
-        horns.render();
-        addOptions();
-
-    });
-});
+let numberOfPage = 1;
+function switchPages(numberOfPage){
+    $(`.page-${numberOfPage}`).on("click",function(){
+        $('main').empty();
+        $('select').empty();
+        $.get(`data/page-${numberOfPage}.json`).then(data => {
+            data.forEach(element => {
+                let horns = new Horns(element.image_url, element.title, element.description, element.keyword, element.horns)
+                horns.render();
+                addOptions();
+            });
+        });
+    })
+}
+switchPages(1);
+switchPages(2);
 
 
 var hornsArr = [];
 var optionsArr = [];
+var optionsArr2 = [];
 
 function Horns(image_url, title, description, keyword, horns) {
     this.image_url = image_url;
@@ -23,15 +31,21 @@ function Horns(image_url, title, description, keyword, horns) {
     hornsArr.push(this);
 };
 
-Horns.prototype.render = function () {
-    let itemCloned = $('.photo-template').clone();
-    itemCloned.removeClass('photo-template');
-    itemCloned.attr('class', this.keyword);
-    itemCloned.find('img').attr("src", this.image_url);
-    itemCloned.find('h2').text(this.title);
-    itemCloned.find('p').text(this.description);
-    $('main').append(itemCloned);
+// Horns.prototype.render = function () {
+//     let itemCloned = $('.photo-template').clone();
+//     itemCloned.removeClass('photo-template');
+//     itemCloned.attr('class', this.keyword);
+//     itemCloned.find('img').attr("src", this.image_url);
+//     itemCloned.find('h2').text(this.title);
+//     itemCloned.find('p').text(this.description);
+//     $('main').append(itemCloned);
 
+// };
+
+Horns.prototype.render = function () {
+    let mustachTemplate =$('#horns-template').html();
+    let newItem = Mustache.render(mustachTemplate,this);
+    $('main').append(newItem);
 };
 
 
